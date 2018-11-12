7cc7d3addd869b41145bc252c9f1d4bdb1c7715d
Elias Kanoga Ndegwa <42963150+Kanogaelias@users.noreply.github.com>
2018-11-12 16:11:58 +0300

    export key

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export NEWS_API_KEY='453b76dcd9e346d69dbb99eafce13a2a'
-export SECRET_KEY='123456789987456321'
+export NEWS_API_KEY='280eeda904ca4adf9cf394e4f4729bbe'
+export SECRET_KEY='12345678'
 
 python3.6 manage.py server
